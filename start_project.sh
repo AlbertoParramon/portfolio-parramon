c44ee6bc7ae890aee9728a834b768bda322fb425
AlbertoParramon <parramon.castillo.alberto@gmail.com>
2025-08-31 12:00:39 +0200

    fix start_project.sh

--- start_project.sh
+++ start_project.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 NAME=$1
+MODE=$2
 
 if [[ -d src/assets/$NAME ]]; then
   cp src/assets/$NAME/* src/assets/
-  ng serve -o
+  if [[ $MODE == "PRO" ]]; then
+    ng build --configuration production  
+  else
+    ng serve -o
+  fi
 fi
 
